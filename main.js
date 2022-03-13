c8f0ca56db8f42ab7f41d9751672271e93fc6cf7
bint-muktar <obsehassen2018@gmail.com>
2022-03-12 23:25:44 -0700

    added icon

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+
+let city = "Edmonton";
 let apiKey ="1ed27858cc58c0b5dc29f2d3e7adefb1"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Edmonton&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
 
@@ -20,7 +22,8 @@ function displayTemperature(response){
     document.querySelector(".wind").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector(".date").innerHTML = formatDate(response.data.dt *1000);
     let iconElement = document.querySelector(".icon");
-    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/04n.png`);
+    iconElement.setAttribute("src", 
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`);
     
  }
 
@@ -30,3 +33,4 @@ axios.get(apiUrl).then(displayTemperature);
 
 
 
+ 
